f84e9a0a9d01ea8947481a68e5e7bd561cc8615c
mohammedsadeghi <sadeghimohammad4488@gmail.com>
2022-05-02 01:16:04 +0430

    toggle button style & logic implemented

---
+++ src/components/RadioBtn.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./../assets/style/RadioButton.css";
+
+export const RadioBtn = () => {
+  const [allowed, setAllowed] = React.useState(false);
+  const changeAllowed = () => {
+    setAllowed(!allowed);
+  };
+  return (
+    <div class="h-screen flex items-center justify-center">
+      <div class="relative inline-block w-10 mr-2 align-middle select-none transition duration-200 ease-in">
+        <input
+          type="checkbox"
+          name="toggle"
+          id="toggle"
+          onChange={changeAllowed}
+          class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
+        />
+        <label
+          for="toggle"
+          class={`toggle-label block overflow-hidden h-6 rounded-full ${
+            allowed ? "bg-green-500" : "bg-red-500"
+          } cursor-pointer`}
+        ></label>
+      </div>
+      <label for="toggle" class="text-xs text-gray-700">
+        {allowed ? (
+          <span className=" text-green-500 ">Allowed</span>
+        ) : (
+          <span className=" text-red-500 ">Blocked</span>
+        )}
+      </label>
+    </div>
+  );
+};
