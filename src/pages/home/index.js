c42619a3c4a9ba2738b31e74c8f2240ac8760c92
Marzieh Valizadeh <sureh1937@gmail.com>
2022-05-02 00:16:41 +0430

    feat: fetch data from mock-server

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,32 @@
+import { useQuery } from 'react-query'
+
 const Home = () => {
+
+    async function fetchPlugins() {
+        const response = await fetch(
+            "http://localhost:3001/items"
+        );
+        const data = await response.json();
+        return data
+    }
+
+    const { isLoading, error, data } = useQuery('pluginData', fetchPlugins)
+
     return (
-        <div>
-            <h1>Home page</h1>
+        <div className='container'>
+            {
+                isLoading ? <p>Loading ...</p>
+                    : error ? <p>Error! {error.message}</p>
+                        : (
+                            data &&
+                            data.length &&
+                            data.map((plugin) => {
+                                return <li key={plugin.id}>{plugin.title}</li>
+                            })
+                        )
+            }
         </div>
-    );
+    )
 }
 
 export default Home;
