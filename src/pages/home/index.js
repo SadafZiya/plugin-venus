524b0979286043390826af53ff626ba38c0a1c85
Marzieh Valizadeh <sureh1937@gmail.com>
2022-05-02 19:39:23 +0430

    add card component to home page with server data

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,19 @@
 import { useQuery } from 'react-query'
 import Header from '../../components/Header'
+import Card from "../../components/Card"
+
 const Home = () => {
 
     async function fetchPlugins() {
-        const response = await fetch(
-            "http://localhost:3001/items"
-        );
+        const response = await fetch("http://localhost:3001/items");
         const data = await response.json();
-        return data
+        return data;
     }
 
     const { isLoading, error, data } = useQuery('pluginData', fetchPlugins)
 
     return (
-        <>
+        <div className=''>
             <Header title='Marketing Plugins' />
             <div className='container'>
                 {
@@ -23,12 +23,12 @@ const Home = () => {
                                 data &&
                                 data.length &&
                                 data.map((plugin) => {
-                                    return <li key={plugin.id}>{plugin.title}</li>
+                                    return <Card data={plugin} key={plugin.id} />
                                 })
                             )
                 }
             </div>
-        </>
+        </div>
     )
 }
 
