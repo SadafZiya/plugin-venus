73784372a02f6a53fc39cd5a73a5fef2b26fd7ad
unknown <zia.s69@gmail.com>
2022-05-04 00:35:15 +0430

    finally merge with component-card and plugin-page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { useQuery } from "react-query";
 import Header from "../../components/Header";
 import Card from "../../components/Card";
@@ -12,7 +13,7 @@ const Home = () => {
     const { isLoading, error, data } = useQuery("pluginData", fetchPlugins);
 
     // Update one plugin based on isAllowed item
-    const updatePluginAllowed = async (isAllowed, id) => {
+    const updatePluginAllow = async (isAllowed, id) => {
         const { data } = await patchOneData(id, { isAllowed: !isAllowed });
         return data;
     };
@@ -30,11 +31,7 @@ const Home = () => {
                     data.length &&
                     data.map((plugin) => {
                         return (
-                            <Card
-                                cardInfo={plugin}
-                                key={plugin.id}
-                                updatePluginAllowed={updatePluginAllowed}
-                            />
+                            <Card data={plugin} key={plugin.id} updateOnToggle={updatePluginAllow} />
                         );
                     })
                 )}
