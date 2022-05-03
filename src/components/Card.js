6395b9b6e148dd0eb4935c53f608a5432735439d
unknown <Crypto.devloper3@gmail.com>
2022-05-03 19:18:41 +0430

    add tooltip pass data to radio button check data and add id

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,19 +3,43 @@ import React from "react";
 import "../assets/style/CardComponent.css";
 import { RadioBtn } from "./RadioBtn.js";
 
-function Card({ data }) {
+function Card({ cardInfo, key, updatePlugin }) {
   return (
     <div
+      id={key}
       className={`w-1/4 rounded-md border border-gray-400 inline-flex m-4 box-border ${
-        data.isDisabled ? "disabledbutton" : ""
+        cardInfo.isDisabled ? (cardInfo.isDisabled ? "disabledbutton" : "") : ""
       }`}
     >
       <div className="w-10/12 flex flex-col items-start p-4">
-        <h2 className="font-medium text-lg">{data.title}</h2>
-        <h3 className="text-left pt-6 text-gray-400">{data.description}</h3>
+        <h2 className="font-medium text-lg">
+          {cardInfo.title ? cardInfo.title : ""}
+        </h2>
+        <h3 className="text-left pt-6 text-gray-400 tooltip">
+          {cardInfo.description
+            ? cardInfo.description.length >= 50
+              ? cardInfo.description.substr(0, 50) + "..."
+              : cardInfo.description
+            : ""}
+          <span
+            class={`tooltiptext ${
+              cardInfo.description
+                ? cardInfo.description.length >= 50
+                  ? ""
+                  : "hidden"
+                : ""
+            }`}
+          >
+            {cardInfo.description
+              ? cardInfo.description.length >= 50
+                ? cardInfo.description
+                : ""
+              : ""}
+          </span>
+        </h3>
       </div>
       <div className="pt-4">
-        <RadioBtn />
+        <RadioBtn cardInfo={cardInfo} updatePlugin={updatePlugin} />
       </div>
     </div>
   );
