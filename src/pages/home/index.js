e6fb52c7bad48616525aad4a590feb95a95cacb1
Marzieh Valizadeh <sureh1937@gmail.com>
2022-05-03 20:45:27 +0430

    refactor pages/home/index.js

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,49 +1,42 @@
-import { useQuery } from 'react-query'
-import Header from '../../components/Header'
-import Card from "../../components/Card"
+import { useQuery } from "react-query";
+import Header from "../../components/Header";
+import Card from "../../components/Card";
+import { getAllData, patchOneData } from "../../services/CRUDServices";
 
 const Home = () => {
-
-    async function fetchPlugins() {
-        const response = await fetch("http://localhost:3001/items");
-        const data = await response.json();
+    // Get all plugins
+    const fetchPlugins = async () => {
+        const { data } = await getAllData();
         return data;
-    }
+    };
+    const { isLoading, error, data } = useQuery("pluginData", fetchPlugins);
 
-    function updatePlugin(isAllowed, id) {
-        fetch(`http://localhost:3001/items/${id}`, {
-            method: 'PATCH',
-            body: JSON.stringify({
-                isAllowed: !isAllowed,
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        })
-            .then((response) => response.json())
-            .then((json) => console.log(json))
-    }
-
-    const { isLoading, error, data } = useQuery('pluginData', fetchPlugins)
+    // Update one plugin based on isAllowed item
+    const updatePluginAllow = async (isAllowed, id) => {
+        const { data } = await patchOneData(id, { isAllowed: !isAllowed });
+        return data;
+    };
 
     return (
-        <div className=''>
-            <Header title='Marketing Plugins' />
-            <div className='container'>
-                {
-                    isLoading ? <p>Loading ...</p>
-                        : error ? <p>Error! {error.message}</p>
-                            : (
-                                data &&
-                                data.length &&
-                                data.map((plugin) => {
-                                    return <Card data={plugin} key={plugin.id} updatePlugin={updatePlugin} />
-                                })
-                            )
-                }
+        <div className="">
+            <Header title="Marketing Plugins" />
+            <div className="container">
+                {isLoading ? (
+                    <p>Loading ...</p>
+                ) : error ? (
+                    <p>Error! {error.message}</p>
+                ) : (
+                    data &&
+                    data.length &&
+                    data.map((plugin) => {
+                        return (
+                            <Card data={plugin} key={plugin.id} updateOnToggle={updatePluginAllow} />
+                        );
+                    })
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
