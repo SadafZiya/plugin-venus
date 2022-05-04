45bd0fba487ad7f09cd5f97589129eb9c64dc264
unknown <zia.s69@gmail.com>
2022-05-05 01:22:35 +0430

    correct radio button toggle

--- src/components/ToggleButton.jsx
+++ src/components/ToggleButton.jsx
@@ -8,20 +8,20 @@ const ToggleButton = ({data, updateOnToggle, refetch}) => {
                 <input
                     type="checkbox"
                     name={`${data.id}`}
-                    id={`${data.id}`}
+                    id={`rd-${data.id}`}
                     onChange={() => updateOnToggle(data, refetch)}
                     checked={data?.isAllowed}
                     disabled={data?.isDisabled}
                     className="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 appearance-none cursor-pointer"
                 />
                 <label
-                    htmlFor={`${data.id}`}
+                    htmlFor={`rd-${data.id}`}
                     className={`toggle-label block overflow-hidden h-6 rounded-full ${
                         data?.isAllowed ? "bg-green-500" : "bg-red-500"
                     } cursor-pointer`}
                 ></label>
             </div>
-            <label htmlFor={`${data.id}`} className="text-sm text-gray-700">
+            <label htmlFor={`rd-${data.id}`} className="text-sm text-gray-700">
                 {data?.isAllowed ? (
                     <span className=" text-green-500 ">Allowed</span>
                 ) : (
