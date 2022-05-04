bee2742a53bb8c2f8f7c7d7f398933a95e3250c3
alizabetpoor <ali_0_zabetpoor@ymail.com>
2022-05-04 11:10:08 +0430

    add not found routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
-import Home from "./pages/home"
-import PluginPage from "./pages/plugin"
+import Home from "./pages/home";
+import PluginPage from "./pages/plugin";
+import NotFoundPage from "./pages/notfoundpage";
 
 const routes = [
+    { path: "*", element: <NotFoundPage /> },
     { path: "/", element: <Home /> },
     { path: "/plugin/:id", element: <PluginPage /> },
-]
+];
 export default routes;
